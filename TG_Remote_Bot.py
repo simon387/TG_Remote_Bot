3a85c779e2b0d9ec4943b6ddfbe523db7e4dfcca
Simone Celia <simone.celia@simonecelia.it>
2023-06-09 16:35:55 +0200

    test

--- TG_Remote_Bot.py
+++ TG_Remote_Bot.py
@@ -39,7 +39,17 @@ async def send_cmd(update: Update, context: CallbackContext):
 	if Constants.EMPTY == cmd:
 		return await context.bot.send_message(chat_id=update.effective_chat.id, text=Constants.ERROR_PARAMETER_NEEDED_MESSAGE)
 	try:
-		output = subprocess.check_output(cmd, shell=True, timeout=Constants.CMD_TIMEOUT)
+		if cmd.startswith("sudo "):
+			cmd.lstrip("sudo ")
+			sudo_command = ['sudo', '-S'] + cmd.split()
+			process = subprocess.Popen(sudo_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+			password = '12Caratteri!\n'
+			process.stdin.write(password)
+			process.stdin.flush()
+			#
+			output, error = process.communicate()
+		else:
+			output = subprocess.check_output(cmd, shell=True, timeout=Constants.CMD_TIMEOUT)
 		if output != b'':
 			return await send_msg_w(update, context, output.decode(Constants.UTF_8, errors='ignore'))
 		return await context.bot.send_message(chat_id=update.effective_chat.id, text=Constants.MESSAGE_CMD_EXECUTED)
